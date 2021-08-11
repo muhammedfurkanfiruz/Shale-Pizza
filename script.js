9470b09ed0c83ab8743e75e7a6e2754085b0143d
muhammedfurkanfiruz <mfurkan.firuz@gmail.com>
2021-08-11 19:10:22 +0300

    pizza ordering page was created with bootstrap.

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    //banner 
+    $("#banner-slider .owl-carousel").owlCarousel({
+        items:1,
+        loop: true , 
+        autoplay:true,
+        nav: true
+    });
+} );
